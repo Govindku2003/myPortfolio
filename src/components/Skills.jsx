a5b0a028fd68d213c628e385abc7aea9ab043c58
Govind kumar kushwaha <152367763+Govindku2003@users.noreply.github.com>
2026-07-08 19:26:50 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import { skills } from '../assets/assets'
 
 const Skills = () => {
   return (
-    <motion.div  
+    <motion.div 
     initial={{opacity:0,y:50}}
          whileInView={{opacity:1,y:0}}
          transition={{duration:0.5 ,ease:'easeInOut'}}
